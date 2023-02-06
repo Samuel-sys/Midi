fcea2b912ef4f8f852dfd988425ae2e54551be72
Samuel-sys <Alencar5647@gmail.com>
2023-02-06 03:32:00 -0300

    Projeto final do curso

--- aluramidi-curso-arquivos-iniciais/script/main.js
+++ aluramidi-curso-arquivos-iniciais/script/main.js
@@ -4,11 +4,28 @@ const teclas = document.querySelectorAll('.tecla');
 teclas.forEach(element => {
 
     element.addEventListener('click',
-    //Pegamos o nome da classe do botão adicionamos o '#som_' mais o resto do nome da classe do botão
-    //assim obtemos o nome da classe do som que queremos ouvir
-    () => playSom(`#som_${element.classList[1]}`));
+        //Pegamos o nome da classe do botão adicionamos o '#som_' mais o resto do nome da classe do botão
+        //assim obtemos o nome da classe do som que queremos ouvir
+        () => playSom(`#som_${element.classList[1]}`));
+
+    element.onkeydown = (e) => {
+        if (e.code === 'Enter' || e.code === 'Space') element.classList.toggle('ativa');
+    }
+    element.onkeyup = (e) => {
+        if (e.code === 'Enter' || e.code === 'Space') element.classList.toggle('ativa');
+    }
 });
 
-function playSom(element) {
-    document.querySelector(element).play();
+function playSom(seletorAudio) {
+
+    const element = document.querySelector(seletorAudio);
+
+    //Verifica se o elemento e ou não uma tag de audio
+    if (element.localName === 'audio') {
+        element.play();
+    }
+    //Verifica se o elemento voltou nulo ou não no sistema
+    else {
+        console.log(`Elemento n'ao encontrado`)
+    }
 }
